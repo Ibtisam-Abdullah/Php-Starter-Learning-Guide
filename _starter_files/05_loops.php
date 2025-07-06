a90ae7369eee59a821280883d59db0e1aea500f6
Ibtisam Abdullah Al Hinai <ibtisam.abdullah998@gmail.com>
2025-07-06 05:27:06 +0400

    Update 05_loops.php

--- _starter_files/05_loops.php
+++ _starter_files/05_loops.php
@@ -1,48 +1,67 @@
 <?php
 // =====================================
-//  joonguini and joe
+//  Learn with Codency
 // =====================================
 
-/* -------- Loops & Iteration ------- */
+/* -------- Loops & Iteration in PHP --------
 
-/* ------------ For Loop ------------ */
+Loops allow you to repeat blocks of code based on conditions.
+PHP supports several types of loops:
+1. for
+2. while
+3. do...while
+4. foreach (used with arrays)
+*/
 
-/*
-** For Loop Syntax
-  for (initialize; condition; increment) {
-  // code to be executed
+/* ------------ For Loop ------------
+
+Used when the number of iterations is known.
+Syntax:
+  for (initialization; condition; increment) {
+    // code to run
   }
 */
 
+for ($i = 1; $i <= 5; $i++) {
+    echo "Step $i<br>";
+}
 
-/* ------------ While Loop ------------ */
+/* ------------ While Loop ------------
 
-/*
-** While Loop Syntax
-  while (condition) {
-  // code to be executed
-  }
+Repeats as long as the condition is true.
+Useful when you don't know how many times you'll loop.
 */
 
+$count = 1;
+while ($count <= 3) {
+    echo "Count is $count<br>";
+    $count++;
+}
 
-/* ---------- Do While Loop --------- */
+/* ---------- Do...While Loop ----------
 
-/*
-** Do While Loop Syntax
-  do {
-  // code to be executed
-  } while (condition);
-
-do...while loop will always execute the block of code once, even if the condition is false.
+Runs the block at least once, even if the condition is false.
 */
 
+$num = 1;
+do {
+    echo "Number is $num<br>";
+    $num++;
+} while ($num <= 2);
 
+/* ---------- Foreach Loop ----------
 
-/* ---------- Foreach Loop ---------- */
-
-/*
-** Foreach Loop Syntax
+Specially used to loop through arrays.
+Syntax:
   foreach ($array as $value) {
-  // code to be executed
+    // code to run
   }
 */
+
+$fruits = ["Apple", "Banana", "Mango"];
+
+foreach ($fruits as $fruit) {
+    echo "I like $fruit<br>";
+}
+
+?>
