383e6a48e66c0537901e7c86ac96e6abf7094bc8
Ibtisam Abdullah Al Hinai <ibtisam.abdullah998@gmail.com>
2025-07-06 08:43:11 +0400

    Update 08_string_functions.php

--- _starter_files/08_string_functions.php
+++ _starter_files/08_string_functions.php
@@ -1,10 +1,53 @@
 <?php
 // =====================================
-//  joonguini and joe
+//  Learn with Codency
 // =====================================
-/* ---------- String Functions -------- */
 
-/*
-  Functions to work with strings
-  https://www.php.net/manual/en/ref.strings.php
+/* ---------- String Functions in PHP ----------
+
+PHP has a wide range of string functions that make text manipulation easy.
+
+Official reference: https://www.php.net/manual/en/ref.strings.php
+Below are more commonly used string functions:
 */
+
+/* -------- ucwords() --------
+
+Capitalizes the first letter of each word.
+*/
+
+$title = "learn php with codency";
+echo ucwords($title); // Output: Learn Php With Codency
+
+/* -------- ucfirst() --------
+
+Capitalizes only the first character of a string.
+*/
+
+echo ucfirst("php is awesome"); // Output: Php is awesome
+
+/* -------- nl2br() --------
+
+Converts newlines (\n) to HTML <br> tags.
+Useful when displaying text from textareas in HTML.
+*/
+
+$paragraph = "This is line one.\nThis is line two.";
+echo nl2br($paragraph);
+// Output: This is line one.<br>This is line two.
+
+/* -------- strrev() --------
+
+Reverses a string.
+*/
+
+echo strrev("codency"); // Output: ycnedoc
+
+/* -------- str_repeat() --------
+
+Repeats a string a specified number of times.
+*/
+
+echo str_repeat("=", 10); // Output: ==========
+
+/* -------- htmlspecialch*
