45c49ae847203243cfea0af31b5679c5737f54ef
Ibtisam Abdullah Al Hinai <ibtisam.abdullah998@gmail.com>
2025-07-06 05:24:25 +0400

    Update 04_conditionals.php

--- _starter_files/04_conditionals.php
+++ _starter_files/04_conditionals.php
@@ -1,38 +1,72 @@
 <?php
 // =====================================
-//  joonguini and joe
+//  Learn with Codency
 // =====================================
 
 /* ---- Conditionals & Operators ---- */
 
-/* ------------ Operators ----------- */
+/* ------------ Comparison Operators ------------
 
-/*
-  < Less than
-  > Greater than
-  <= Less than or equal to
-  >= Greater than or equal to
-  == Equal to
-  === Identical to
-  != Not equal to
-  !== Not identical to
+Used to compare values and return true or false.
 */
 
-/* ---------- If & If-Else Statements --------- */
+$a = 10;
+$b = 5;
 
-/*
-** If Statement Syntax
-if (condition) {
-  // code to be executed if condition is true
+// Basic comparison examples
+var_dump($a < $b);   // false
+var_dump($a > $b);   // true
+var_dump($a <= 10);  // true
+var_dump($a >= 11);  // false
+var_dump($a == "10");  // true (value equal, type doesn't matter)
+var_dump($a === "10"); // false (not the same type)
+var_dump($a != $b);   // true
+var_dump($a !== 10);  // true (different type)
+
+/* ------------ If & If-Else Statements ------------
+
+Used to run code based on conditions.
+*/
+
+$score = 85;
+
+if ($score >= 90) {
+    echo "Excellent!";
+} elseif ($score >= 75) {
+    echo "Good job!";
+} else {
+    echo "Keep trying!";
 }
+// Output: Good job!
+
+/* ------------ Ternary Operator ------------
+
+A short version of an if-else statement.
+Syntax:
+  condition ? result_if_true : result_if_false;
 */
 
-/* -------- Ternary Operator -------- */
-/*
-  The ternary operator is a shorthand if statement.
-  Ternary Syntax:
-    condition ? true : false;
+$age = 18;
+echo ($age >= 18) ? "Adult" : "Minor"; // Output: Adult
+
+/* ------------ Switch Statements ------------
+
+Useful when checking a variable against multiple values.
 */
 
+$day = "Monday";
 
-/* -------- Switch Statements ------- */
+switch ($day) {
+    case "Monday":
+        echo "Start of the week!";
+        break;
+    case "Friday":
+        echo "Weekend is near!";
+        break;
+    default:
+        echo "Just another day.";
+        break;
+}
+// Output: Start of the week!
+
+?>
